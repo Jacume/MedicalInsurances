1017958373249e09624bf2c79d98460cc5954558
John Cruz <john.cruz@seedburysquare.com>
2025-11-04 08:02:35 -0400

    applied use state to track values

--- src/components/labeledInput/LabeledInput.tsx
+++ src/components/labeledInput/LabeledInput.tsx
@@ -6,7 +6,7 @@
  * @copyright Copyright (c) 2025
  */
 
-import { HTMLInputTypeAttribute } from 'react';
+import { HTMLInputTypeAttribute, type ChangeEventHandler} from 'react';
 import './labeledInput.css';
 
 function LabeledInput({
@@ -14,15 +14,16 @@ function LabeledInput({
 	color,
 	label,
 	type = 'text',
-	children
+	children,
+	onChange
 }: {
 	size?: string;
 	color?: string;
 	label?: string;
 	type?: HTMLInputTypeAttribute;
 	children?: string;
+	onChange?: ChangeEventHandler
 }) {
-
 	return (
 		<label className={`labeledInput-view labeledInput-view_${size} labeledInput-view_${color}`}>
 			<span className={`labeledInput-label labeledInput-label_${size} labeledInput-label_${color}`}>{label}</span>
@@ -30,6 +31,7 @@ function LabeledInput({
 				type={type}
 				className={`labeledInput-input labeledInput-input_${size} labeledInput-input_${color}`}
 				value={children}
+				onChange={onChange}
 			/>
 		</label>
 	);
--- src/views/contact/Contact.tsx
+++ src/views/contact/Contact.tsx
@@ -6,24 +6,31 @@
  * @copyright Copyright (c) 2025
  */
 
+import { useState, type FormEventHandler } from 'react';
 import LabeledInput from '../../components/labeledInput/LabeledInput.tsx';
 import './contact.css';
 
 function Contact() {
-	/** @type FormEventHandler<HTMLFormElement> */
-	const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+	const [firstName, setFirstName] = useState('');
+	const [secondName, setSecondName] = useState('');
+	const [firstLastName, setLastFirstName] = useState('');
+	const [secondLastName, setSecondLastName] = useState('');
+	const [phoneNumber, setPhoneNumber] = useState('');
+	const [email, setEmail] = useState('');
+
+	const onSubmit: FormEventHandler = (e) => {
 		e.preventDefault();
-		console.log('asdf:', e);
+		console.log(':', firstName, secondName, firstLastName, secondLastName, phoneNumber, email);
 	};
 	return (
 		<form className={`contact-view`} onSubmit={onSubmit}>
-			<LabeledInput label='Nombre'></LabeledInput>
-			<LabeledInput label='Segundo Nombre'></LabeledInput>
-			<LabeledInput label='Apellido'></LabeledInput>
-			<LabeledInput label='Segundo Apellido'></LabeledInput>
-			<LabeledInput label='Número de Teléfono' type='tel'></LabeledInput>
-			<LabeledInput label='Email'></LabeledInput>
-			<button type='submit'></button>
+			<LabeledInput label='Nombre' onChange={(e) => {setFirstName(e.target.nodeValue ?? '')}}>{firstName}</LabeledInput>
+			<LabeledInput label='Segundo Nombre' onChange={(e) =>{setSecondName(e.target.nodeValue ?? '');}}>{secondName}</LabeledInput>
+			<LabeledInput label='Apellido' onChange={(e) =>{setLastFirstName(e.target.nodeValue ?? '');}}>{firstLastName}</LabeledInput>
+			<LabeledInput label='Segundo Apellido' onChange={(e) => {setSecondLastName(e.target.nodeValue ?? '');}}>{secondLastName}</LabeledInput>
+			<LabeledInput label='Número de Teléfono' type='tel' onChange={(e) => {setPhoneNumber(e.target.nodeValue ?? '');}}>{phoneNumber}</LabeledInput>
+			<LabeledInput label='Email' onChange={(e) => {setEmail(e.target.nodeValue ?? '');}}>{email}</LabeledInput>
+			<button type='submit'>Someter</button>
 		</form>
 	);
 }
